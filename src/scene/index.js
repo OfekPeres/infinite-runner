e1b4ba303e3f095a9201988eff041f3edd1bbc4f
Ofek Peres <Ofek@Ofeks-MacBook-Pro.local>
2020-05-07 22:53:45 -0400

    trying to fix merge conflicts...

--- src/scene/index.js
+++ src/scene/index.js
@@ -20,66 +20,6 @@ const main = () => {
     canvas.focus();
     const engine = new Engine(canvas, true);
     const scene = createScene(engine);
-    createLighting(scene);
-    // const platforms = createInfiniteTrack(scene);
-    const player = new Player(scene);
-    createCamera(scene, canvas, player);
-    // const testPlatform = new Platform(scene, new Vector3(0, -5, 0), {width: 100, height: 1, depth: 100});
-    // const testLane = new Lane(scene, 4, new Vector3(0, -5, 0), {width: 40, height: 1, depth: 50});
-    const game = new Game(scene, player, 3);
-    // for (let i = 0; i < 3; i++)
-    // {
-    //     createRandomBox(scene);
-    // }
-    // createRotatingBox2(scene);
-    handleKeyboard(scene, game);
-    handleResize(engine);
-
-    createTrampoline(scene);
-
-    createTrampoline(scene);
-
-    createTrampoline(scene);
-
-    createTrampoline(scene);
-
-    createTrampoline(scene);
-
-    createTrampoline(scene);
-
-    engine.runRenderLoop(function() {
-        // updateInfiniteTrack(platforms, player.playerBox.position.z);
-        // for (let i = 0; i < 3; i ++) {
-        //     const lane = platforms[i];
-        //     for (const platform of lane)
-        //     {
-        //         if (player.playerBox.intersectsMesh(platform.platform)){
-        //             player.jumps = 0
-        //         }
-        //         if (platform.hasLauncher && player.playerBox.intersectsMesh(platform.launcher)){
-        //         player.jumps = 0
-        //         const impulse  = new Vector3(0, 300, 0);
-
-        //         player.playerBox.physicsImpostor.applyImpulse(impulse, player.playerBox.getAbsolutePosition());
-        //         // platform.launcher.physicsImpostor.setLinearVelocity(new Vector3(0, 10, 0));
-        //         // platform.launcher.physicsImpostor.setAngularVelocity(new Vector3(0, 0, 0));
-        //         // if (platform.launcher.position.y > 10)
-        //         // {
-        //         //     platform.resetLauncher();
-        //         // }
-        //     }
-
-        // }
-    // }
-    // if (player.playerBox.position.y < -40)
-    // {
-    //     console.log("Reset Player");
-    //     player.resetPlayer();
-    //     // player.playerBox.position.y = 20;
-    // }
-        game.update();
-        scene.render();
-    });
 
     // Load Assets (3D Models, etc)
     const assetsManager = new AssetsManager(scene);
@@ -97,6 +37,7 @@ const main = () => {
 
     assetsManager.useDefaultLoadingScreen = true;
     engine.loadingUIText = "Loading 3D Models! Game will be ready soon!";
+
     assetsManager.onFinish = () => {
 
         // Initialize player and game objects once all assets have been loaded
@@ -104,6 +45,7 @@ const main = () => {
         const player = new Player(scene);
         createCamera(scene, canvas, player);
         const game = new Game(scene, player, 3);
+
         handleKeyboard(scene, game);
         handleResize(engine);
 
